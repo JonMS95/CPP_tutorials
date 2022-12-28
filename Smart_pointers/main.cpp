1aa91259ad31914597de720e417a9c788386983e
JonMS95 <jon_portugalete@hotmail.com>
2022-12-28 15:42:52 +0100

    Created main file content.

---
+++ Smart_pointers/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <memory>
+// If smart pointers are going to be used, then "memory" file should be included.
+
+int main()
+{
+    
+
+    return 0;
+}
