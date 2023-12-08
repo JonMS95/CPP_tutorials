3a8d7432e6fce0ab55183bf3a6ac093e95813862
JonMS95 <jon_portugalete@hotmail.com>
2023-12-08 18:24:33 +0100

    Added a lesson about template classes and functions in C++.

--- Templates/Source_files/Templates.hpp
+++ Templates/Source_files/Templates.hpp
@@ -3,6 +3,8 @@
 
 /********* Include statements *********/
 
+#include <iostream>
+
 /**************************************/
 
 // The concept of template in C++ imay be applied both for functions and classes. Templates in C++ are basically
@@ -19,9 +21,48 @@
 //}
 
 // Something that is worth pointing out is that templates must be defined within header files instead of cpp files.
-// the reason behind this is that linker should find the proper implementation for the instantiated types.
+// the reason behind this is that linker should find the proper implementation for the instantiated types. In fact,
+// the implementation of these template function will depend on the usage it is done of them.
+
+// Appart from functions, template classes can be defined as well, which is, by the way, how most of the classes in
+// the STL (Standard Template Library) are implemented, as the name suggests.
+
+// In the template class example shown below, a custom array class is described. It will take two template arguments:
+// firstly, the type of which the array will be, as it should be composed by different variable types, such as strings,
+// integers, floating point numbers or whatever. Secondly, the size of the target array should be passed between
+// triangular brackets. This way, it can be guaranteed that the array will be created in stack memory (same as it
+// happens in std::array template class).
+
+// Same as it happens with template fuctions, template classes should be fully defined within the header files, as the
+// linker, again, should know which specific function is meant to be implemented depending on the type that is being used.
 
 /********** Class prototypes **********/
+
+template<typename T, int size> class TemplateArray
+{
+private:
+    T m_template_array[size]; // This stands for an array of class "T" and size "size".
+    std::string m_name;
+    const int m_size = size;
+
+public:
+    TemplateArray(void):m_name("DEFAULT_NAME"){}
+    
+    TemplateArray(std::string name_input):
+        m_name(name_input){}
+    
+    std::string GetName(void) { return this->m_name; }
+
+    T& operator[](int index)
+    {
+        if(index < 0 || index > size)
+        {
+            return this->m_template_array[0];
+        }
+
+        return this->m_template_array[index];
+    }
+};
 
 /**************************************/
 
@@ -32,6 +73,14 @@
 template<typename T> T TemplateAdd(T a, T b)
 {
     return a + b;
+}
+
+// The explicit type the template function should work with can also be explicitly told, instead of using typename
+// variables, which make the function usable with (almost) any variable type. This does not make much sense, but it
+// just can exist.
+template<int number> void TemplatePrintInt()
+{
+    std::cout << number << std::endl;
 }
 
 /**************************************/
--- Templates/Source_files/main.cpp
+++ Templates/Source_files/main.cpp
@@ -1,6 +1,7 @@
 /********* Include statements *********/
 
 #include <iostream>
+#include <string>
 #include "Templates.hpp"
 
 /**************************************/
@@ -25,7 +26,34 @@ void TestTemplates()
 
     std::cout << "3 + 4 = " << TemplateAdd(3, 4) << std::endl;
     std::cout << "2.3 + 4.9 = " << TemplateAdd(2.3, 4.9) << std::endl;
-    std::cout << "Hello + World = " << TemplateAdd((std::string)"Hello", (std::string)"World") << std::endl;
+
+// If it is not specified anywhere which the target type to be treated is, then it will be implicitly
+// casted by the template function. Nevertheless, it can be explicitly told by using triangular brackets,
+// same as it is done when declaring the type a vector will contain (such as std::vector<int> for integers).
+// In the case shown below, it is necessary to tell that the target type is std::string. Otherwise,
+// const char* will be assumed, which is a type that does not admit the "+" operator overloading operation.
+
+    std::cout << "Hello + World = " << TemplateAdd<std::string>("Hello", "World") << std::endl;
+
+    TemplatePrintInt<7>();
+
+    TemplateArray<std::string, 3> template_array_0;
+    TemplateArray<int, 4> template_array_1("Jon");
+
+    std::cout << "template_array_0.GetName() = " << template_array_0.GetName() << std::endl;
+    std::cout << "template_array_1.GetName() = " << template_array_1.GetName() << std::endl;
+
+    template_array_0[0] = "Hello";
+    template_array_0[1] = ", ";
+    template_array_0[2] = "World!";
+
+    template_array_1[0] = 1;
+    template_array_1[1] = 9;
+    template_array_1[2] = 9;
+    template_array_1[3] = 5;
+
+    std::cout << ( template_array_0[0] + template_array_0[1] + template_array_0[2] ) << std::endl;
+    std::cout << "Birth date: " << template_array_1[0] << template_array_1[1] << template_array_1[2] << template_array_1[3] << std::endl;
 }
 
 int main()
