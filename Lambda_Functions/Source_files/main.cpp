8fd42d3a82217051ba24c5bdd67c0a503f65e9af
JonMS95 <jon_portugalete@hotmail.com>
2024-10-05 15:15:24 +0200

    Added lambda function usage example when applying it to standard library sorting algorithm.

--- Lambda_Functions/Source_files/main.cpp
+++ Lambda_Functions/Source_files/main.cpp
@@ -1,17 +1,22 @@
 /********* Include statements *********/
 
 #include <iostream>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 #include "BasicLambdaFunctions.hpp"
 #include "DummyLambdaFunctionsClass.hpp"
+#include "SortWithLambdaFunction.hpp"
 
 /**************************************/
 
 /********** Define statements *********/
 
-#define MSG_TEST_HEADER                 "*********************************"
-#define MSG_TEST_LAMBDA_FUNCTIONS       "Testing Lambda functions."
-#define MSG_TEST_BASIC_LAMBDA_FUNCTIONS "Testing basic Lambda functions."
-#define MSG_TEST_CLASS_LAMBDA_FUNCTIONS "Testing Lambda functions in classes."
+#define MSG_TEST_HEADER                     "*********************************"
+#define MSG_TEST_LAMBDA_FUNCTIONS           "Testing Lambda functions."
+#define MSG_TEST_BASIC_LAMBDA_FUNCTIONS     "Testing basic Lambda functions."
+#define MSG_TEST_CLASS_LAMBDA_FUNCTIONS     "Testing Lambda functions in classes."
+#define MSG_TEST_SORT_WITH_LAMBDA_FUNCTIONS "Testing standard library sorting algorithm with lambda functions."
 
 /**************************************/
 
@@ -34,6 +39,32 @@ void TestLambdaFunctionsInClasses()
     d1.updateAge(43);
 }
 
+void TestSortWithLambdaFunctions()
+{
+    std::vector<int> vec1;
+    srand (time(NULL));
+
+    for(int i = 0; i < 10; i++)
+    {
+        vec1.emplace_back(rand() % 100 + 1);
+    }
+
+    std::vector<int> vec2 = vec1;
+
+    auto print_vector = [](std::vector<int>& vec) -> void
+    {
+        for(int n:vec)
+            std::cout << n << " ";
+        std::cout << std::endl;
+    };
+
+    sortInAscendingOrder(vec1);
+    sortInDescendingOrder(vec2);
+
+    print_vector(vec1);
+    print_vector(vec2);
+}
+
 void TestLambdaFunctions()
 {
     PrintTestHeader(MSG_TEST_LAMBDA_FUNCTIONS);
@@ -43,6 +74,9 @@ void TestLambdaFunctions()
 
     PrintTestHeader(MSG_TEST_CLASS_LAMBDA_FUNCTIONS);
     TestLambdaFunctionsInClasses();
+
+    PrintTestHeader(MSG_TEST_SORT_WITH_LAMBDA_FUNCTIONS);
+    TestSortWithLambdaFunctions();
 }
 
 int main()
