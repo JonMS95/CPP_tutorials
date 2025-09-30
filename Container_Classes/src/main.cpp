a07016ca44eed691e6e8bc9fc91c63472fc3e6fc
JonMS95 <jon_portugalete@hotmail.com>
2025-09-30 20:36:51 +0200

    Added lessons about stacks and queues (container adaptors). Heaps are yet to be covered.

--- Container_Classes/src/main.cpp
+++ Container_Classes/src/main.cpp
@@ -1,11 +1,16 @@
 /********* Include statements *********/
 
 #include <iostream>
+
 #include <array>
 #include <vector>
 #include <deque>
 #include <list>
 #include <forward_list>
+
+#include <stack>
+#include <queue>
+#include <algorithm>
 
 /**************************************/
 
@@ -13,9 +18,12 @@
 
 #define MSG_TEST_HEADER                 "*********************************"
 #define MSG_TEST_SEQUENCE_CONTAINERS    "Testing sequence containers."
-#define MSG_TEST_CONTAINER_ADAPTERS     "Testing container adapters."
+#define MSG_TEST_CONTAINER_ADAPTORS     "Testing container adaptors."
 #define MSG_TEST_ASSOCIATIVE_CONTAINERS "Testing associative containers."
 #define MSG_TEST_UNORDERED_CONTAINERS   "Testing unordered containers."
+
+#define PRINT_SEQ_CONT(VAR_NAME)    printSequenceContainer(#VAR_NAME, VAR_NAME)
+#define PRINT_CONT_ADAPT(VAR_NAME)  printContainerAdaptor(#VAR_NAME, VAR_NAME)
 
 /**************************************/
 
@@ -27,19 +35,19 @@ void PrintTestHeader(const char* header)
 }
 
 template <typename T>
-void printSequenceContainer(const std::string var_name, const T& var, const int var_size = -1)
+void printSequenceContainer(const std::string var_name, const T& var)
 {
     std::cout << var_name << " = {";
 
     int idx = 0;
 
     // Works even if container has no .size() (like forward_list).
-    const int max_size = std::distance(var.begin(), var.end());
+    const int var_size = std::distance(var.begin(), var.end());
 
     for(const auto& v : var)
     {
         std::cout << v;
-        if(idx++ < (max_size - 1))
+        if(idx++ < (var_size - 1))
             std::cout << ", ";
     }
 
@@ -87,7 +95,7 @@ void TestSequenceContainers(void)
     dq.emplace_back(3); // Same as with vectors, emplace_ or push_ can be used preferrably depending on the case.
     dq.emplace_front(1);
 
-    printSequenceContainer("dq", dq);
+    PRINT_SEQ_CONT(dq);
 
     // Lists: doubly-linked lists. each elemented is linked to both the previous and the following.
     // No random access (it must be traversed). It can be traversed both forward or backwards since
@@ -98,7 +106,7 @@ void TestSequenceContainers(void)
     dlist.pop_back();
     dlist.emplace_front('a'); // Again, emplace_ or push_ methods can be used alongside doubly linked lists (depending on the case). 
     dlist.emplace_back('c');
-    printSequenceContainer("dlist", dlist);
+    PRINT_SEQ_CONT(dlist);
 
     // Forward lists: singly-linked lists. Can only be traversed in a single direction (forward).
     // A bit lighter than doubly-linked lists (just a single pointer vs two). Memory efficient.
@@ -120,12 +128,124 @@ void TestSequenceContainers(void)
     flist.remove("Sonntag");
     flist.insert_after(std::find(flist.begin(), flist.end(), "Samstag"), "Domingo");
 
-    printSequenceContainer("flist", flist);
+    PRINT_SEQ_CONT(flist);
+}
+
+template <typename T>
+void printContainerAdaptor(const std::string& var_name, const T& var)
+{
+    std::cout << var_name << " = {";
+
+    T var_copy = var;
+
+    while(!var_copy.empty())
+    {
+        std::cout << var_copy.top();
+        var_copy.pop();
+        
+        if(!var_copy.empty())
+            std::cout << ", ";
+    }
+
+    std::cout << "}" << std::endl;
+}
+
+void TestContainerAdaptors(void)
+{
+    // Stack: LIFO queue. Wraps std::deque by default, but can use vector or even list.
+    // Only allows operations that make sense for a stack: push, pop, top, empty, size.
+    std::stack<int> st_0;
+
+    for(int i = 0; i < 5; i++)  // Push elements to the stack.
+        st_0.push(i);
+
+    auto print_int_stack = [](const std::string& st_name, std::stack<int> st) -> void
+    {
+        std::vector<int> temp_vec;
+        
+        while(!st.empty())
+        {
+            temp_vec.emplace_back(st.top());
+            st.pop();
+        }
+
+        printSequenceContainer(st_name, temp_vec);
+    };
+
+    print_int_stack("st_0", st_0);
+    
+    std::stack<int> st_1 = static_cast<std::stack<int>>(std::deque<int>{5, 6, 7, 8, 9}); // stacks can be initialized from deques.
+    
+    print_int_stack("st_1", st_1);
+
+    while(!st_1.empty())
+        st_1.pop();
+
+    // Queue: FIFO queue. Again, wraps a deque by default. Supports push, pop, front,
+    // back, emtpy, size.
+    std::queue<int> q_0;
+    for(int i = 10; i < 20; i++)
+        q_0.push(i);
+    
+    for(int i = 0; i < q_0.size() / 2; i++)
+        q_0.pop();
+    
+    auto print_int_queue = [](const std::string& q_name, std::queue<int> q) -> void
+    {
+        std::vector<int> temp_vec;
+        
+        while(!q.empty())
+        {
+            temp_vec.emplace_back(q.front());
+            q.pop();
+        }
+
+        printSequenceContainer(q_name, temp_vec);
+    };
+
+    print_int_queue("q_0", q_0);
+
+    std::queue<int> q_1 = static_cast<std::queue<int>>(std::deque{1, 2, 3});    // Queues can also be initialized from deques.
+    print_int_queue("q_1", q_1);
+
+    // Priority queue: heap (priority ordering). Wraps std::vector by default and
+    // mantains heap property. Always keeps the largest element on top by default.
+    // A heap is a special type of binary tree (usually represented as an array)
+    // which usually satisfies the heap property:
+    // ·Max-heap: every parent node is greater than or equal to its children.
+    // ·Min-heap: every parent node is less than or equal to its children.
+    // Example:
+    //         42
+    //       /    \
+    //      29     18
+    //     /  \   /  \
+    //    14  7  18  12
+    // This could be stored in an array: {42, 29, 18, 14, 7, 18, 12}
+    // If a heap is stored as an array, then:
+    // Parent index:    i
+    // Left child:      2 * i + 1
+    // Right child:     2 * i + 2
+
+    // Let's say we have the following numbers: {10, 3, 25, 8, 15, 2}, which can
+    // be represented as a binary tree as follows:
+    //     10
+    //    /  \
+    //   3    25
+    //  / \   /
+    // 8  15  2
+    std::vector<int> tree_values = {10, 3, 25, 8, 15, 2};
+    std::priority_queue<int> h_0;
+    
+    for(int n : tree_values)
+        h_0.push(n);
+    
+    // printSequenceContainer("h_0", static_cast<std::vector<int>>(h_0));
 }
 
 int main()
 {
     TestSequenceContainers();
+    TestContainerAdaptors();
 
     return 0;
 }
