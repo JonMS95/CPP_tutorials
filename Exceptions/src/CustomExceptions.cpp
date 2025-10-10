587f97dddb501c15626c158546019c62ab8905cd
JonMS95 <jon_portugalete@hotmail.com>
2025-10-10 18:56:00 +0200

    Extended the explanation on the derived class extension common procedure.

--- Exceptions/src/CustomExceptions.cpp
+++ Exceptions/src/CustomExceptions.cpp
@@ -22,14 +22,34 @@
 //     virtual const char* what() const noexcept;
 // };
 //
-// Since the methods shown are public, they can be overridden on condition the qualifiers are mantained.
+// Since the methods shown are public, they can be overridden on condition the qualifiers are mantained
+// (except possibly virtual but override instead, in case it's not meant to be derived anymore).
 // An alternative (and popular) choice is to call the base constructor in the derived class constructor
 // especially in those cases in which the base class constructor takes a string as input parameter.
+//
+// Typical derived class' what() method prototype should look like follows:
+//
+// const char* what() const noexcept override
+//
+// Let's split it into pieces:
+// ·const char*: returns a constant character string (C-style).
+// ·const: since the method modifies nothing, const qualifier should be used.
+// ·noexcept: the method should not throw any exception.
+// ·override: as the base class method is virtual, it should be overriden in the derived class.
 
 /***** Private function prototypes ****/
 
 /**************************************/
 
 /******** Function definitions *********/
+
+class MyException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+
+    }
+};
 
 /**************************************/
