96819024eb2a9d9e9b827555f0a96bc12edeb644
JonMS95 <jon_portugalete@hotmail.com>
2025-10-10 18:50:46 +0200

    Added an explanation on the ideas to be covered.

--- Exceptions/src/CustomExceptions.cpp
+++ Exceptions/src/CustomExceptions.cpp
@@ -7,16 +7,29 @@
 
 /********* Define statements *********/
 
-
-
 /**************************************/
+
+// This is how base class exception is defined (more or less):
+//
+//  class exception
+// {
+//   public:
+//     exception() noexcept { }
+//     virtual ~exception() noexcept;
+//
+//     // Default copy constructor, assignment operator overload, move constructor here.
+//
+//     virtual const char* what() const noexcept;
+// };
+//
+// Since the methods shown are public, they can be overridden on condition the qualifiers are mantained.
+// An alternative (and popular) choice is to call the base constructor in the derived class constructor
+// especially in those cases in which the base class constructor takes a string as input parameter.
 
 /***** Private function prototypes ****/
 
 /**************************************/
 
 /******** Function definitions *********/
-
-
 
 /**************************************/
