320700cb0ece21347260365d15a1a3512786750c
JonMS95 <jon_portugalete@hotmail.com>
2025-10-11 12:50:36 +0200

    Added files so as to explain upcoming lesson.

---
+++ Exceptions/src/Noexcept.cpp
@@ -0,0 +1,31 @@
+/********* Include statements *********/
+
+#include <stdexcept>
+#include "Common.hpp"
+#include "Noexcept.hpp"
+
+/**************************************/
+
+
+
+/********* Include statements *********/
+
+
+
+/**************************************/
+
+// noexcept is a specifier taht tells the compiler and fellow developers whether a function guarantees not to
+// throw any exception. Functions marked as noexcept must not throw: throwing from them leads to std::terminate.
+// In other words, it's a way to tell that C++ exception mechanism is not allowed for a given function.
+
+/***** Private function prototypes ****/
+
+
+
+/**************************************/
+
+/******** Function definitions ********/
+
+
+
+/**************************************/
---
+++ Exceptions/src/Noexcept.hpp
@@ -0,0 +1,10 @@
+#ifndef NOEXCEPT_HPP
+#define NOEXCEPT_HPP
+
+/******** Function prototypes *********/
+
+
+
+/**************************************/
+
+#endif
