1f24dc44c1bd3c945aaee428ed033f814609db8f
JonMS95 <jon_portugalete@hotmail.com>
2025-10-11 21:14:31 +0200

    Reorganized the file.

--- Type_Traits/src/TypeTraitsIntro.cpp
+++ Type_Traits/src/TypeTraitsIntro.cpp
@@ -43,6 +43,15 @@ void demoBasicTypeTraits(void)
     // Print true/false instead of 1/0.
     std::cout << std::boolalpha;
 
+    // Some dummy variables and type definitions to be used later.
+    int var_int = 1;
+    int var_float = 1.1f;
+    
+    class myClass{};
+    typedef struct{} myStruct;
+    enum myEnum { A, B, C };
+    union myUnion {int i; float f; };
+
     // Check integral types. std::is_integral will return true for all types in C++ that represent whole integer types.
     {
         COMP_CHECK_TYPE(integral);
@@ -50,39 +59,36 @@ void demoBasicTypeTraits(void)
         COMP_FAMILY_TYPES(char,        integral, std::is_integral);
         COMP_FAMILY_TYPES(double,      integral, std::is_integral);
         COMP_FAMILY_TYPES(std::string, integral, std::is_integral);
-        int var_int = 1;
-        int var_float = 1.1f;
+        
         COMP_FAMILY_TYPES(decltype(var_int),   integral, std::is_integral);
         COMP_FAMILY_TYPES(decltype(var_float), integral, std::is_integral);
     }
     // Check floating-point types. std::is_floating_point will return true for all types in C++ that represent floating-point numbers.
     {
         COMP_CHECK_TYPE(floating-point);
+
         COMP_FAMILY_TYPES(float, floating-point, std::is_floating_point);
         COMP_FAMILY_TYPES(double, floating-point, std::is_floating_point);
         COMP_FAMILY_TYPES(int, floating-point, std::is_floating_point);
         COMP_FAMILY_TYPES(std::string, floating-point, std::is_floating_point);
-        int var_int = 1;
-        int var_float = 1.1f;
         COMP_FAMILY_TYPES(decltype(var_int),   integral, std::is_floating_point);
-        COMP_FAMILY_TYPES(decltype(var_float), integral, std::is_floating_point);
+        COMP_FAMILY_TYPES(decltype(var_float), integral, std::is_floating_point);        
     }
     // It can also be checked whether a given variable is a pointer or not, regardless of the pointed variable's type. Use std::is_pointer.
     {
         COMP_CHECK_TYPE(pointer);
+
         COMP_FAMILY_TYPES(int*, int-pointer, std::is_pointer);
-        COMP_FAMILY_TYPES(int, int-pointer, std::is_pointer);
+        COMP_FAMILY_TYPES(int,  int-pointer, std::is_pointer);
     }
     // Exact types can be checked as well: std::is_same<T, U>::value. Since C++17, std::is_same_v<T, U> can be used so as to avoid ::value field retrieval.
     // Use decltype alongside the variable in question to retrieve its type.
     {
         COMP_CHECK_TYPE(int);
-        class myClass{};
-        typedef struct{} myStruct;
-        int var_int = 1;
-        int var_float = 1.1f;
+
         myClass dummy_class;
         myStruct dummy_struct;
+        
         COMP_TYPES(decltype(var_int),       int,        std::is_same);
         COMP_TYPES(decltype(var_float),     int,        std::is_same);
         COMP_TYPES(decltype(dummy_class),   myClass,    std::is_same);
