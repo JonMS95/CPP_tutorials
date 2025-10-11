3a75fc31b93650a39a7942f973c7103af101854e
JonMS95 <jon_portugalete@hotmail.com>
2025-10-11 19:29:28 +0200

    Added the basic structure of the chapter on type traits.

---
+++ Type_Traits/src/TypeTraitsIntro.hpp
@@ -0,0 +1,8 @@
+#ifndef TYPE_TRAITS_INTRO_HPP
+#define TYPE_TRAITS_INTRO_HPP
+
+/******** Function prototypes *********/
+
+/**************************************/
+
+#endif
--- Type_Traits/src/main.cpp
+++ Type_Traits/src/main.cpp
@@ -1,22 +1,14 @@
 /********* Include statements *********/
 
 #include <iostream>
-#include "BasicExceptions.hpp"
-#include "ExceptionTypes.hpp"
-#include "IOExceptions.hpp"
-#include "CustomExceptions.hpp"
-#include "Noexcept.hpp"
+#include "TypeTraitsIntro.hpp"
 
 /**************************************/
 
 /********** Define statements *********/
 
-#define MSG_TEST_HEADER                 "*********************************"
-#define MSG_TEST_BASIC_EXCEPTIONS       "Testing basic exceptions."
-#define MSG_TEST_EXCEPTION_TYPES        "Testing exception types."
-#define MSG_TEST_FILE_IO_EXCEPTIONS     "Testing file I/O exceptions."
-#define MSG_TEST_CUSTOM_EXCEPTIONS      "Testing custom exceptions."
-#define MSG_TEST_NOEXCEPT_KEYWORD       "Testing noexcept keyword."
+#define MSG_TEST_HEADER             "*********************************"
+#define MSG_TEST_TYPE_TRAITS_INTRO  "Testing basic type traits."
 
 /**************************************/
 
@@ -27,64 +19,16 @@ static void PrintTestHeader(const char* header)
     std::cout << std::endl << MSG_TEST_HEADER << std::endl << header << std::endl << MSG_TEST_HEADER << std::endl;
 }
 
-static void TestBasicExceptions(void)
+static void TesttypeTraitsIntro(void)
 {
-    PrintTestHeader(MSG_TEST_BASIC_EXCEPTIONS);
+    PrintTestHeader(MSG_TEST_TYPE_TRAITS_INTRO);
 
-    throwingBuiltInTypes();
-    throwStandardException();
-    try2divide();
-    exceptionHierarchy();
-}
 
-static void TestExceptionTypes(void)
-{
-    PrintTestHeader(MSG_TEST_EXCEPTION_TYPES);
-
-    causeGenericLogicErrorException();
-    causeInvalidArgumentException();
-    causeOutOfRangeException();
-    causeDomainErrorException();
-    causeLengthErrorException();
-
-    causeGenericRuntimeException();
-    causeRangeErrorException();
-    causeOverflowException();
-    causeUnderflowException();
-}
-
-static void TestFileIOExceptions(void)
-{
-    PrintTestHeader(MSG_TEST_FILE_IO_EXCEPTIONS);
-
-    printFileContent();
-}
-
-static void TestCustomExceptions(void)
-{
-    PrintTestHeader(MSG_TEST_CUSTOM_EXCEPTIONS);
-
-    causeCustomException();
-    causeFileNotFoundException();
-    causeInvalidAgeException();
-}
-
-static void TestNoexceptkeyword(void)
-{
-    PrintTestHeader(MSG_TEST_NOEXCEPT_KEYWORD);
-
-    sayHello("Hello");
-    exceptionPropagationWithNoexcept();
-    testConditionalExceptions();
 }
 
 int main()
 {
-    TestBasicExceptions();
-    TestExceptionTypes();
-    TestFileIOExceptions();
-    TestCustomExceptions();
-    TestNoexceptkeyword();
+    TesttypeTraitsIntro();
 
     return 0;
 }
