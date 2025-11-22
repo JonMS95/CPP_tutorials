5c90b8b6bfa8a5b8c838818c0cd6aec3040bdfea
Jon <jon_portugalete@hotmail.com>
2025-11-22 19:55:44 +0100

    Added an example explaning jthread basics.

---
+++ Threads/src/JThreads.hpp
@@ -0,0 +1,10 @@
+#ifndef JTHREADS_HPP
+#define JTHREADS_HPP
+
+/******** Function prototypes *********/
+
+void incrementCounterWithTwoJThreads(void);
+
+/**************************************/
+
+#endif
