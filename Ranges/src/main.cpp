2b5ea787171dd26c0242d9526d3a1c43a132e4bb
Jon <jon_portugalete@hotmail.com>
2025-12-29 19:15:38 +0100

    Added some examples about views (filter/transformer) in C++.

--- Ranges/src/main.cpp
+++ Ranges/src/main.cpp
@@ -1,7 +1,11 @@
 /********* Include statements *********/
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <string>
 #include "BasicRanges.hpp"
+#include "Views.hpp"
 
 /**************************************/
 
@@ -9,6 +13,7 @@
 
 #define MSG_TEST_HEADER         "*********************************"
 #define MSG_TEST_BASIC_RANGES   "Testing basic ranges."
+#define MSG_TEST_VIEWS          "Testing views."
 
 /**************************************/
 
@@ -16,6 +21,15 @@
 
 static void PrintTestHeader(const char* header);
 static void TestBasicRanges(void);
+static void TestViews(void);
+
+/**************************************/
+
+/********** Global variables **********/
+
+const std::vector<int> vec_int{9, 4, 3, 3, 9, 5, 5, 9, 2, 10};
+const std::list<std::string> list_str{"Hello", "world", "of", "ranges"};
+const std::string str = "abc";
 
 /**************************************/
 
@@ -30,15 +44,31 @@ static void TestBasicRanges(void)
 {
     PrintTestHeader(MSG_TEST_BASIC_RANGES);
 
-    range_basics::iterateVector();
-    range_basics::iterateList();
-    range_basics::iterateString();
+    range_basics::iterateVector(vec_int);
+    range_basics::iterateList(list_str);
+    range_basics::iterateString(str);
     range_basics::iterateTemporaryRange();
 }
+
+static void TestViews(void)
+{
+    PrintTestHeader(MSG_TEST_VIEWS);
+
+    const std::vector<int> even_numbers = views::getEvenNumbers(vec_int);
+    range_basics::iterateVector(even_numbers);
+    
+    const std::string uppercase_str = views::toUppercase(str);
+    range_basics::iterateString(uppercase_str);
+
+    const std::vector<int> doubled_odd_numbers = views::getDoubledOddNumbers(vec_int);
+    range_basics::iterateVector(doubled_odd_numbers);
+}
+
 
 int main()
 {
     TestBasicRanges();
+    TestViews();
 
     return 0;
 }
