ea6e0bd517a91e579e3d38d4684337b7cdd50a3b
Jon <jon_portugalete@hotmail.com>
2025-12-29 19:20:20 +0100

    Added the skeleton files for upcoming lesson on subranges and slicing in C++.

---
+++ Ranges/src/SubrangesAndSlicing.cpp
@@ -0,0 +1,23 @@
+/********* Include statements *********/
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include "SubrangesAndSlicing.hpp"
+
+/**************************************/
+
+/*
+A subrange is a view of part of a range (without copying elements).
+*/
+
+/******** Function definitions *********/
+
+namespace subranges_and_slicing
+{
+
+
+
+}
+
+/**************************************/
---
+++ Ranges/src/SubrangesAndSlicing.hpp
@@ -0,0 +1,19 @@
+#ifndef SUBRANGES_AND_SLICING_HPP
+#define SUBRANGES_AND_SLICING_HPP
+
+/********* Include statements *********/
+
+#include <vector>
+
+/**************************************/
+
+/******** Function prototypes *********/
+
+namespace subranges_and_slicing
+{
+
+}
+
+/**************************************/
+
+#endif
