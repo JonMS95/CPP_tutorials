55e79af11831e59213c0488cdf2f0a39c602a5de
JonMS95 <jon_portugalete@hotmail.com>
2025-12-30 19:05:51 +0100

    Added function prototypes as well as template functions.

--- Ranges/src/SubrangesAndSlicing.hpp
+++ Ranges/src/SubrangesAndSlicing.hpp
@@ -4,6 +4,8 @@
 /********* Include statements *********/
 
 #include <vector>
+#include <stdexcept>
+#include <ranges>
 
 /**************************************/
 
@@ -11,6 +13,26 @@
 
 namespace subranges_and_slicing
 {
+
+std::vector<int> getFirstNIntegers(const std::vector<int>& vec, const int n);
+std::vector<int> getAllIntegersExceptFirstN(const std::vector<int>& vec, const int n);
+template<typename T> std::vector<T> getTrimmedVector(const std::vector<T>& vec, const int trim_left, const int trim_right)
+{
+    if((trim_left + trim_right) > static_cast<int>(vec.size()))
+        throw std::range_error("Number of elements to be trimmed exceed vector\'s length");
+
+    auto view = vec
+        | std::views::drop(trim_left)
+        | std::views::take(vec.size() - trim_left - trim_right);
+
+    return std::vector<T>(view.begin(), view.end());
+}
+template<typename T> std::vector<T> getRevertedVector(const std::vector<T>& vec)
+{
+    auto view = vec | std::views::reverse;
+    
+    return std::vector<T>(view.begin(), view.end());
+}
 
 }
 
