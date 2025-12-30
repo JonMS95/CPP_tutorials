5f55f38b32cedb721bfa29c60a7e8123bd8dc69b
JonMS95 <jon_portugalete@hotmail.com>
2025-12-30 19:06:02 +0100

    Added some function definitions.

--- Ranges/src/SubrangesAndSlicing.cpp
+++ Ranges/src/SubrangesAndSlicing.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <ranges>
 #include <vector>
+#include <stdexcept>
 #include "SubrangesAndSlicing.hpp"
 
 /**************************************/
 
 /*
-A subrange is a view of part of a range (without copying elements).
+A subrange is a view of part of a range (without copying elements). Views like take and drop can be
+pretty useful when it comes to retrieving solely part of the target range.
 */
 
 /******** Function definitions *********/
@@ -16,7 +18,27 @@ A subrange is a view of part of a range (without copying elements).
 namespace subranges_and_slicing
 {
 
+std::vector<int> getFirstNIntegers(const std::vector<int>& vec, const int n)
+{
+    if(n > static_cast<int>(vec.size()))
+        throw std::range_error("Number of integers to be retrieved is greater than provided vector\'s length.");
+    
+    auto view = vec | std::views::take(n);
+    
+    return std::vector<int>(view.begin(), view.end());
+}
 
+std::vector<int> getAllIntegersExceptFirstN(const std::vector<int>& vec, const int n)
+{
+    if(n > static_cast<int>(vec.size()))
+        throw std::range_error("Number of integers to be skipped is greater than provided vector\'s length.");
+
+    auto view = vec | std::views::drop(n);
+
+    std::vector<int> ret;
+
+    return std::vector<int>(view.begin(), view.end());
+}
 
 }
 
