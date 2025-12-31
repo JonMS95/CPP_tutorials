e3e16042c1cb924bfdd4ac08c886d8549bb5b31e
JonMS95 <jon_portugalete@hotmail.com>
2025-12-31 16:13:48 +0100

    Created basic files for this chapter

---
+++ Coroutines/src/main.cpp
@@ -0,0 +1,45 @@
+/********* Include statements *********/
+
+#include <iostream>
+#include "GeneratorIntro.hpp"
+
+/**************************************/
+
+/********** Define statements *********/
+
+#define MSG_TEST_HEADER             "*********************************"
+#define MSG_TEST_BASIC_GENERATORS   "Testing basic coroutine generators."
+
+/**************************************/
+
+/**** Private function prototypes *****/
+
+static void PrintTestHeader(const char* header);
+static void TestGeneratorIntro(void);
+
+/**************************************/
+
+/********** Global variables **********/
+
+/**************************************/
+
+/******** Function definitions ********/
+
+static void PrintTestHeader(const char* header)
+{
+    std::cout << std::endl << MSG_TEST_HEADER << std::endl << header << std::endl << MSG_TEST_HEADER << std::endl;
+}
+
+static void TestGeneratorIntro(void)
+{
+    PrintTestHeader(MSG_TEST_BASIC_GENERATORS);
+}
+
+int main()
+{
+    TestGeneratorIntro();
+
+    return 0;
+}
+
+/**************************************/
