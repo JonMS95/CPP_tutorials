6c44a9fe938192926cf762237ef2578a81c18c3e
Jon <jon_portugalete@hotmail.com>
2026-01-03 19:28:30 +0100

    Added a teoretical explanation about what co_await is all about.

--- Coroutines/src/AsyncCoroutines.cpp
+++ Coroutines/src/AsyncCoroutines.cpp
@@ -5,7 +5,13 @@
 /**************************************/
 
 /*
+Before starting, let's make a recap about yield, return and await in the context of coroutines:
+·co_yield: pauses coroutine's execution, holding its state and returning a value at a time.
+·co_return: ends coroutine's execution forcefully.
+·co_await: waits for an event to happen out of the coroutine's context.
 
+It's worth pointing out that a coroutine does not generate nor provide anything when calling
+co_await, but wait for something to happen externally.
 */
 
 /******** Function definitions *********/
