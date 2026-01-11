656a9e0ea25ea29e08cc9d4137ef62c4d183ef58
JonMS95 <jon_portugalete@hotmail.com>
2026-01-11 17:35:02 +0100

    Added header file for functor testing functions to be executed from main file.

---
+++ Function_Objects/src/BasicFunctors.hpp
@@ -0,0 +1,16 @@
+#ifndef BASIC_FUNCTORS_HPP
+#define BASIC_FUNCTORS_HPP
+
+/******** Function prototypes *********/
+
+namespace basic_functors
+{
+
+void MinimalFunctorExample(void);
+void FunctorWithState(void);
+
+}
+
+/**************************************/
+
+#endif
